7ad88f2ee6d0265e12de934153ea5e6714398495
ian-lonergan <lonergan.ian@gmail.com>
2014-03-12 17:43:13 -0400

    intial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheGmCompanion::Application.config.secret_key_base = '819660127f4c492e6c1c2a371bc83d7a0ca4af49f243e2b3ebca99378dfe6c176b42ce1efb64e2a84a5ef34a62f61163ce5d299a0394fc7f24e948a5e4382c85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheGmCompanion::Application.config.session_store :cookie_store, key: '_the-gm-companion_session'
