d6a7e1aab6baefbcd2c24f5423284fc274c141bb
ian-lonergan <lonergan.ian@gmail.com>
2014-03-17 22:07:32 -0400

    Now have creation and editing of campaign_objects implemented.

--- app/controllers/campaign_objects_controller.rb
+++ app/controllers/campaign_objects_controller.rb
@@ -5,17 +5,39 @@ class CampaignObjectsController < ApplicationController
     @campaign_object = CampaignObject.first
   end
   
+  def new
+    @campaign_object = CampaignObject.new
+  end
+  
+  def create
+    @campaign_object = CampaignObject.create(campaign_object_params)
+    @campaign_object.save
+  end
+  
   def show
     @campaign_object = CampaignObject.find(params[:id])
   end
   
-  def create
-    @campaign_object = CampaignObject.create(params[:campaign_object])
+  def edit
+    @campaign_object = CampaignObject.find(params[:id])
+  end
+  
+  def update
+    @campaign_object = CampaignObject.find(params[:id])
+    if @campaign_object.update_attributes(campaign_object_params)
+      redirect_to :action => :show, :id => @campaign_object
+    else
+      redirect_to :action => :index
+    end
   end
   
   def destroy
     @deleted = CampaignObject.find(params[:id]).destroy
     @campaign_object = CampaignObject.first
+  end
+  
+  def campaign_object_params
+    params.require(:campaign_object).permit(:name, :abstract, :notes)
   end
   
 end
--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,21 +5,39 @@ class CharactersController < ApplicationController
     @character = Character.includes(:campaign_object).first
   end
   
-  def show
-    @character = Character.includes(:campaign_object).find(params[:id])
+  def new
+    @character = Character.new
   end
   
   def create
     @character = Character.create(params[:character])
+    @character.save
+  end
+  
+  def show
+    @character = Character.includes(:campaign_object).find(params[:id])
+  end
+  
+  def edit
+    @character = Character.find(params[:id])
   end
   
   def update
-    @character
+    @character = Character.find(params[:id])
+    if @character.update_attributes(character_params)
+      redirect_to :action => :show, :id => @character
+    else
+      redirect_to :action => :index
+    end
   end
   
   def destroy
     @deleted = Character.find(params[:id]).destroy
     @character = Character.includes(:campaign_object).first
+  end
+  
+  def character_params
+    params.require(:character).permit(:character_class, :race, :level, :gender, :description)
   end
   
 end
