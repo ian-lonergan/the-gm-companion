c8faaea94da8cdec777c965d10c44533218eb34a
ian-lonergan <lonergan.ian@gmail.com>
2014-03-18 19:14:59 -0400

    Added starter seeds for database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+anya = CampaignObject.create(name: 'Anya Kamen', abstract: 'The worst paladin ever, eventually the worst Inquisitor ever.', notes: 'Her brother is more important that Shane realizes')
+Character.create(campaign_object: anya, character_class: 'Inquisitor', gender: '?', race: 'Human', level: '6', description: 'Her face is horribly mauled from an acid splash.')
+jowan = CampaignObject.create(name: 'Jowan', abstract: 'Creepy and mysterious necromancer.', notes: 'He doesn\'t actually want to harm any of the PCs, but he would in an instant if he had to.')
+Character.create(campaign_object: jowan, character_class: 'Dread Necromancer', gender: 'Male', race: 'Undead', level: '9', description: 'Once upon a time he was sickly, but with his new undead body he always seems preternaturally still.')
