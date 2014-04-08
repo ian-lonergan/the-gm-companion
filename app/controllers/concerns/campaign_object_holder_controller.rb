c46cfe9fdd21855963b1bc0469be02b84196f8d1
ian-lonergan <lonergan.ian@gmail.com>
2014-04-07 20:15:43 -0400

    Fixed a bug where a user couldn't add new campaign objects to their own campaign.

--- app/controllers/concerns/campaign_object_holder_controller.rb
+++ app/controllers/concerns/campaign_object_holder_controller.rb
@@ -7,8 +7,10 @@ module CampaignObjectHolderController
   
   private
     def correct_user
-      @campaign_object = current_user.campaign_objects.find_by(campaign_object_holder_id: params[:id])
-      if @campaign_object.nil?
+      p params
+      item = current_user.campaign_objects.find_by(campaign_object_holder_id: params[:id])
+      item ||= current_user.campaigns.find_by(id: params[:campaign_id])
+      if item.nil?
         flash[:error] = 'You don\'t have access to do this.'
         redirect_back_or :back
       end
