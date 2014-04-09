833b6e9781e89a23f20d7441633303d018b08213
ian-lonergan <lonergan.ian@gmail.com>
2014-04-08 20:37:36 -0400

    Only the owner of a campaign_object should be able to even see the add notes button.

--- app/models/campaign_object.rb
+++ app/models/campaign_object.rb
@@ -2,6 +2,7 @@ class CampaignObject < ActiveRecord::Base
   belongs_to :campaign
   belongs_to :campaign_object_holder, polymorphic: true, dependent: :delete
   has_many :notes
+  has_one :owner, through: :campaign
 
   validates :name, presence: true
   validates :abstract, presence: true
