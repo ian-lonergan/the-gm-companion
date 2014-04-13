dcb7b5cd8e0970f207e23ecac3084dc9c0025492
ian-lonergan <lonergan.ian@gmail.com>
2014-04-13 14:41:22 -0400

    Secret token has been reset and is now sourced from environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TheGmCompanion::Application.config.secret_key_base = '819660127f4c492e6c1c2a371bc83d7a0ca4af49f243e2b3ebca99378dfe6c176b42ce1efb64e2a84a5ef34a62f61163ce5d299a0394fc7f24e948a5e4382c85'
+TheGmCompanion::Application.config.secret_key_base = ENV['SECRET_KEY']
