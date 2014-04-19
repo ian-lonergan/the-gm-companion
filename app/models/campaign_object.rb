f9d72a3dda1ab4cfaf24dde0f37620cccfe0af7d
ian-lonergan <lonergan.ian@gmail.com>
2014-04-19 13:12:15 -0400

    Now shouldn't save campaign_object unless parent saves.

--- app/models/campaign_object.rb
+++ app/models/campaign_object.rb
@@ -6,8 +6,16 @@ class CampaignObject < ActiveRecord::Base
   
   has_attached_file :picture, styles: { original: '600x600>', tiny:'75x75'}
   
+  before_save :check_campaign_object_holder
+  
   scope :by_type,     ->(type)        { where(campaign_object_holder_type: type) }
   scope :by_campaign, ->(campaign_id) { where(campaign_id: campaign_id) }
+  
+  def check_campaign_object_holder
+    if campaign_object_holder.nil?
+      false
+    end
+  end
 
   validates :name, presence: true
   validates :abstract, presence: true
