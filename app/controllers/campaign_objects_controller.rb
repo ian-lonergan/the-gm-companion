1dc018720b0e07bd5d72982f8b6a90868baa582d
ian-lonergan <lonergan.ian@gmail.com>
2014-05-02 19:03:09 -0400

    Updating for a new method for autocompletion that might be fully implemented later.

--- app/controllers/campaign_objects_controller.rb
+++ app/controllers/campaign_objects_controller.rb
@@ -1,36 +1,9 @@
 class CampaignObjectsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :update, :edit, :destroy]
-  before_action :correct_user, only: [:create, :update, :edit, :destroy]
-  
-  def index
-    @campaign_objects = CampaignObject.all
+  def autocomplete_name
+    type = params[:type]
+    data = type.nil? \
+      ? data = CampaignObject.by_campaign(current_campaign.id).where('LOWER(name) LIKE ?', "%#{params[:term].downcase}%").limit(5) \
+      : data = CampaignObject.by_campaign(current_campaign.id).by_type(type).where('LOWER(name) LIKE ?', "%#{params[:term].downcase}%").limit(5)
+    render json: data.map {|item| {label: item.name, id: item.id, campaign_object_holder_id: item.campaign_object_holder_id}}
   end
-  
-  def create
-    @campaign_object = current_campaign.campaign_objects.build(campaign_object_params)
-    if @campaign_object.save
-      redirect_to :action => :show, :id => @campaign_object
-    else
-      render :new
-    end
-  end
-    
-  def update
-    @campaign_object = CampaignObject.find(params[:id])
-    if @campaign_object.update_attributes(campaign_object_params)
-      redirect_to :action => :show, :id => @campaign_object
-    else
-      redirect_to :action => :index
-    end
-  end
-  
-  def destroy
-    @deleted = CampaignObject.find(params[:id]).destroy
-    redirect_to :action => :index
-  end
-  
-  def campaign_object_params
-    params.require(:campaign_object).permit(:name, :abstract, :picture)
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ TheGmCompanion::Application.routes.draw do
   get "/logout" => "sessions#destroy"
   
   resources :campaigns, shallow: true do
-    resources :campaign_objects
     resources :stories
     resources :characters
     resources :locations
@@ -19,9 +18,9 @@ TheGmCompanion::Application.routes.draw do
   
   resources :campaign_objects, shallow: true do
     resources :notes
+    get :autocomplete_name, on: :collection
   end
   
-  resources :campaign_objects, only: [:index, :create, :update, :destroy]
   resources :stories, only: [:index, :edit, :update, :destroy]
   resources :characters, only: [:index, :edit, :update, :destroy]
   resources :locations, only: [:index, :edit, :update, :destroy]
