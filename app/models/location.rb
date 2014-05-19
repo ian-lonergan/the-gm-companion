786c25cd8fd5d7f2014f206952776a0769e6effc
ian-lonergan <lonergan.ian@gmail.com>
2014-05-18 21:55:28 -0400

    Fixed a bug that allowed potential infinite recursion.

--- app/models/location.rb
+++ app/models/location.rb
@@ -12,7 +12,7 @@ class Location < ActiveRecord::Base
   has_attached_file :map, styles: { original: '600x600>', medium: '300x300'}
   
   def full_locations(n=3)
-    {self => children.length > 0 ? children.map {|child| child.full_locations(n-1)} : nil}
+    {self => children.length > 0 && n > 0 ? children.map {|child| child.full_locations(n-1)} : nil}
   end
   
   validates_attachment_content_type :map, content_type: /\Aimage\/.*\Z/
