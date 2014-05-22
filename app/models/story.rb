b01f7e7598f55c68af3823a186757798b786174c
ian-lonergan <lonergan.ian@gmail.com>
2014-05-22 18:08:28 -0400

    Added nested child view to story.

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,5 +4,9 @@ class Story < ActiveRecord::Base
   belongs_to :parent, class_name: "Story", foreign_key: "parent_id"
   has_many :children, class_name: "Story", foreign_key: "parent_id"
   
+  def full_stories(n=3)
+    {self => children.length > 0 && n > 0 ? children.map {|child| child.full_stories(n-1)} : nil}
+  end
+  
   belongs_to :location
 end
